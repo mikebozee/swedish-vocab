2f6d52020ba30d8051f26226cf287116215e0474
Mike Bozee <msbozee@gmail.com>
2016-11-20 12:54:07 -0800

    terminal-based vocab quiz w/ lives/points
    128-char words only, no utf-8 support yet

---
+++ swedish_vocab.py
@@ -0,0 +1,36 @@
+import csv
+import random
+
+with open('english_to_swedish_test.csv', 'r', newline='', encoding='utf-8') as csvFile:
+    reader = csv.reader(csvFile)
+    headers = next(reader)[0:]
+    print(headers[0].upper() + " ~ " + headers[1].upper())
+    print('-' * 20)
+
+    score = 0
+    lives = 3
+
+    for row in reader:
+        engWord = row[0]
+        swdWord = row[1]
+        print("\n")
+        guess = input("What is \"" + engWord + "\" in Swedish? ")
+
+        if guess == swdWord:
+            score += 1
+            print("CORRECT!")
+            print("SCORE: " + str(score))
+            print("LIVES REMAINING: " + str(lives))
+        else:
+            lives -= 1
+            if lives == 0:
+                print("NOPE!")
+                print("SCORE: " + str(score))
+                print("*** GAME OVER ***")
+                break
+            else:
+                print("NOPE!")
+                print("SCORE: " + str(score))
+                print("LIVES REMAINING: " + str(lives))
+
+csvFile.close()
